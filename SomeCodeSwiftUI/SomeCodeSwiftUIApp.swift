8e198e233955295dcc3193bd5dd76cc624741415
mohammad blur <blor1379@gmail.com>
2024-05-27 18:18:06 +0330

    Initial Commit

---
+++ SomeCodeSwiftUI/SomeCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeCodeSwiftUIApp.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SomeCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
