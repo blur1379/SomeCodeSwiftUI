d51f41908582be8bbcac4e8025d86f294df688b5
mohammad blur <blor1379@gmail.com>
2024-05-28 22:17:31 +0330

    :sparkles:  add  UINotificationFeedbackGenerator

--- SomeCodeSwiftUI/HapticView.swift
+++ SomeCodeSwiftUI/HapticView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 class HapticManager {
     static let instance = HapticManager()
     
-   
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
     
     
 }
