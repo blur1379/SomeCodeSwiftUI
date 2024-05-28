75fa77d769093f8728a097e2c605d48249140dcd
mohammad blur <blor1379@gmail.com>
2024-05-28 22:16:07 +0330

    :sparkles: add haptic view and haptic manager

---
+++ SomeCodeSwiftUI/HapticView.swift
@@ -0,0 +1,18 @@
+//
+//  HapticView.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 5/28/24.
+//
+
+import SwiftUI
+
+struct HapticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticView()
+}
