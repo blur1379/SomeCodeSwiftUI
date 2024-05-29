09032bd03eb0b394ce1e52a88f6feb6844889195
mohammad blur <blor1379@gmail.com>
2024-05-29 21:42:48 +0330

    :sparkles: local notification permission

--- SomeCodeSwiftUI/LccalNotification.swift
+++ SomeCodeSwiftUI/LccalNotification.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import UserNotifications
+
+class NotificationManager {
+    static let instance = NotificationManager()
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization { success, error in
+            if let error {
+                print(error)
+            } else {
+                print("success")
+            }
+        }
+    }
+}
 
 struct LccalNotification: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("request permission") {
+            NotificationManager.instance.requestAuthorization()
+        }
     }
 }
 
