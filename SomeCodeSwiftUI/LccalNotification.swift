5917fcacc5952e057306db17839ae4c1ef70b664
mohammad blur <blor1379@gmail.com>
2024-05-29 21:42:33 +0330

    :sparkles: local notification permission

---
+++ SomeCodeSwiftUI/LccalNotification.swift
@@ -0,0 +1,18 @@
+//
+//  LccalNotification.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 5/29/24.
+//
+
+import SwiftUI
+
+struct LccalNotification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LccalNotification()
+}
