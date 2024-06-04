afdb49e4f5b55c84c0774c455ba65f55a0672fdd
mohammad blur <blor1379@gmail.com>
2024-06-04 23:50:53 +0330

    Add codable file

---
+++ SomeCodeSwiftUI/CodableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CodableBootcamp.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 6/4/24.
+//
+
+import SwiftUI
+
+struct CodableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableBootcamp()
+}
