8ce197679b66f2f7c6ae825d49826d6c5c806f25
mohammad blur <blor1379@gmail.com>
2024-06-08 23:54:52 +0330

    :sparkles: add file manager

---
+++ SomeCodeSwiftUI/FileManager.swift
@@ -0,0 +1,18 @@
+//
+//  FileManager.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 6/8/24.
+//
+
+import SwiftUI
+
+struct FileManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileManager()
+}
