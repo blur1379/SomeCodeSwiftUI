f7fc25d9c924e379021f96fe9db052acc75f5826
mohammad blur <blor1379@gmail.com>
2024-06-10 23:08:58 +0330

    :lipstick: add view for file manager

--- SomeCodeSwiftUI/FileManager.swift
+++ SomeCodeSwiftUI/FileManager.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct FileManager: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200,height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save to FM")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                Spacer()
+            }
+            .navigationTitle("File manager")
+        }
     }
 }
 
