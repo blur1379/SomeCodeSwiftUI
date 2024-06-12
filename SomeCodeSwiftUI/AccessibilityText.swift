4478e21ce1c10205f1df61644d52597c79847ff1
mohammad blur <blor1379@gmail.com>
2024-06-12 22:48:02 +0330

    :sparkles: text accessibility added

---
+++ SomeCodeSwiftUI/AccessibilityText.swift
@@ -0,0 +1,22 @@
+//
+//  AccessibilityText.swift
+//  SomeCodeSwiftUI
+//
+//  Created by Mohammad Blur on 6/12/24.
+//
+
+import SwiftUI
+
+struct AccessibilityText: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    AccessibilityText()
+}
