b6d46b5c835bec6347f89d4ace2ed415de5eee87
mohammad blur <blor1379@gmail.com>
2024-06-12 23:14:28 +0330

    :sparkles: add sizeCategory

--- SomeCodeSwiftUI/AccessibilityText.swift
+++ SomeCodeSwiftUI/AccessibilityText.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AccessibilityText: View {
+    @Environment(\.sizeCategory) var sizeCategory
+    
     var body: some View {
         NavigationStack {
             List {
@@ -16,10 +18,36 @@ struct AccessibilityText: View {
                         HStack {
                             Image(systemName: "heart.fill")
                                 .font(.system(size: 20))
+                            
+                            Text("Welcome to my app")
+                                .truncationMode(.tail)
                         }
+                        .font(.title)
+                        
+                        Text("This text is some longer than before for test the main of this accessibility ")
+                            .font(.subheadline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .lineLimit(3)
+                            .minimumScaleFactor(sizeCategory.customMinScaleFactor)
                     }
                 }
             }
+            .navigationTitle("Accessibility")
+        }
+    }
+}
+
+extension ContentSizeCategory {
+    var customMinScaleFactor: CGFloat {
+        switch self {
+        case .extraSmall, .small, .medium:
+            return 1.0
+            
+        case .large, .extraLarge, .extraExtraLarge:
+            return 0.8
+            
+        default:
+            return 0.9
         }
     }
 }
